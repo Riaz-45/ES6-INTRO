9c2b47505d3e6ee05e5e8203ca9c5f4d3a9f3470
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-09 16:09:01 +0600

    added destructing file

---
+++ destructuring.js
@@ -0,0 +1,38 @@
+// const product = { name: 'shirt', price: 500, quantity: 7};
+
+const { price, quantity, tax ='7%' } = { name: 'shirt', price: 500, quantity: 7};
+
+console.log(price, quantity, tax)
+
+// const discount = product.price*20/100;
+// const yourPay = product.price - discount;
+// const vatAmount = product.price*7/100;
+// const totalAmount = yourPay + vatAmount;
+
+
+// const price = product.price;
+
+// const discount = price*20/100;
+// const yourPay = price - discount;
+// const vatAmount = price*7/100;
+// const totalAmount = yourPay + vatAmount;
+
+
+
+const discount = price*20/100;
+const yourPay = price - discount;
+const vatAmount = price*7/100;
+const totalAmount = yourPay + vatAmount;
+
+
+const device = { name: 'phone', brand: 'samsung', price: 45000};
+const {name} = device;
+
+
+// ----------------------------------------------------- //
+
+const numbers = [25, 88, 89, 100];
+const [first, second, third] = numbers;
+// console.log(first);
+const [math, physics] = [86, 88];
+console.log(physics)
