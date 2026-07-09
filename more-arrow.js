9300cc3fe5b3a640aa06c105870e99d829da8d0b
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-09 15:14:29 +0600

    added more arrow function file

---
+++ more-arrow.js
@@ -0,0 +1,24 @@
+const getTax = (amount, taxRate) => amount*taxRate/100;
+
+
+// single parameter
+const getSquare = (x) => x*x;
+const getHalf = num => num/2;
+const firstElement = nums => nums[0];
+const logIt = () => console.log(76);
+
+console.log(getSquare(8))
+console.log(getHalf(8))
+
+console.log(firstElement([22, 33, 55]))
+logIt()
+console.log(logIt())
+
+
+
+// annonymus function
+document.getElementById('').addEventListener('click', () => {
+    
+})
+
+
--- tempCodeRunnerFile.js
+++
@@ -1,3 +0,0 @@
-n add(num1, num2){
-//     return num1 + num2;
-// }
