39e87df9b3dbe2d79af7b289bc00c10c0330978c
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-11 11:47:27 +0600

    added entries file

---
+++ entries.js
@@ -0,0 +1,32 @@
+const employee = {
+    name: 'Rajib',
+    designation: 'developer',
+    salary: 20000,
+    experience: 1
+}
+
+
+// object er key sokol key name ber korar jonno use hoy
+const keys = Object.keys(employee);
+// console.log(keys);
+
+
+// object er sokol value ber korar jonno
+const values = Object.values(employee);
+// console.log(values);
+
+
+// value increment process
+employee.salary = employee.salary + 5000;
+
+const entries = Object.entries(employee);
+
+// object property delete process
+delete employee.experience;
+delete employee.salary;
+
+// object ke freeze kore rakhte freeze property--->ekhetre object er kono kichui change kora jabe na
+Object.freeze(employee)
+
+console.log(entries);
+console.log(employee)
